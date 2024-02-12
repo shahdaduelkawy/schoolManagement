d29d4dd96e73a29bb76239dc58ac4e4d304dcef1
shahdaduelkawy <you@example.com>
2024-02-12 23:55:33 +0200

    final

--- schoolManagement/routes/SchoolRoute.js
+++ schoolManagement/routes/SchoolRoute.js
@@ -16,14 +16,14 @@ const {
   addMediaArray,
 } = require("../services/SchoolService");
 
-//const authService = require("../services/authService");
+const authService = require("../services/authService");
 
 const router = express.Router();
 
-//router.use(authService.protect);
+router.use(authService.protect);
 
 // Admin
-//router.use(authService.allowedTo("superAdmin"));
+router.use(authService.allowedTo("superAdmin"));
 router.route("/").get(getSchool).post(createSchoolValidator, createSchool);
 router.route("/delete/:id").delete(deleteSchoolValidator, deleteSchool);
 router.route("/Search/:id").get(getSearchValidator, getSearch);
